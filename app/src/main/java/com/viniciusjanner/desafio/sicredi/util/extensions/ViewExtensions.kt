6335fe936c13f4d84f78549d060f8440088ac80b
Vinicius Janner <viniciusjanner@gmail.com>
2023-10-07 15:13:55 -0300

    Add new method in ViewExtensions file.

--- app/src/main/java/com/viniciusjanner/desafio/sicredi/util/extensions/ViewExtensions.kt
+++ app/src/main/java/com/viniciusjanner/desafio/sicredi/util/extensions/ViewExtensions.kt
@@ -3,6 +3,15 @@ package com.viniciusjanner.desafio.sicredi.util.extensions
 import android.view.View
 
 @Suppress("unused")
+fun View.onSingleClick(delayMillis: Long = 700L, action: (v: View) -> Unit) {
+    this.setOnClickListener { v ->
+        isClickable = false
+        action(v)
+        postDelayed({ isClickable = true }, delayMillis)
+    }
+}
+
+@Suppress("unused")
 fun View.visibilityGone() {
     this.visibility = View.GONE
 }
