98e9e9ee91792c5d82de42ae37a8f51a46066c6b
Vinicius Janner <viniciusjanner@gmail.com>
2023-10-14 17:58:57 -0300

    Rename file (MarginItemDecoration to MarginItemDecorationPortrait).

--- app/src/main/java/com/viniciusjanner/desafio/sicredi/presentation/common/MarginItemDecorationPortrait.kt
+++ app/src/main/java/com/viniciusjanner/desafio/sicredi/presentation/common/MarginItemDecorationPortrait.kt
@@ -4,27 +4,28 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class MarginItemDecoration(
+class MarginItemDecorationPortrait(
     private var marginTop: Int,
-    private var marginLeft: Int,
-    private var marginRight: Int,
     private var marginBottom: Int,
-    private var marginBottomLast: Int = marginBottom
+    private var marginBottomLast: Int = marginBottom,
+    private var marginRight: Int,
+    private var marginLeft: Int,
 ) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+
         with(outRect) {
             val position = parent.getChildAdapterPosition(view)
 
             // Verifica se é o primeiro item
             if (position == 0) top = marginTop
 
+            // Verifica se é o último item
+            bottom = if (position == state.itemCount - 1) marginBottomLast else marginBottom
+
             left = marginLeft
 
             right = marginRight
-
-            // Verifica se é o último item
-            bottom = if (position == state.itemCount - 1) marginBottomLast else marginBottom
         }
     }
 }
